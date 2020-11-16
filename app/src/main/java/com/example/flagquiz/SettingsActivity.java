4314c59be8695bf1fc89d37563ab83f641593764
vitocolerone <18520701@gm.uit.edu.vn>
2020-11-16 12:30:50 +0700

    Uploadfile

---
+++ app/src/main/java/com/example/flagquiz/SettingsActivity.java
@@ -0,0 +1,19 @@
+// SettingsActivity.java
+// Activity to display SettingsActivityFragment on a phone
+package com.example.flagquiz;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class SettingsActivity extends AppCompatActivity {
+   // inflates the GUI, displays Toolbar and adds "up" button
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_settings);
+      Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+      setSupportActionBar(toolbar);
+      getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+   }
+}
